b084f76e7a3472ebe27dd459c2b2797b3657f90f
kodaimura <kodaimura123@gmail.com>
2022-11-24 23:33:41 +0900

    add p9,10,11

--- project_euler.clj
+++ project_euler.clj
@@ -1,4 +1,6 @@
-(ns project.euler)
+(ns project.euler
+  (:require
+    [clojure.string :as string]))
 
 
 ;p1
@@ -172,7 +174,7 @@
 ;p8
 ;largest product in a series
 
-(def p8numstr
+(def p8arg*
  "96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
 12540698747158523863050715693290963295227443043557
@@ -201,23 +203,138 @@
 (defn multiply-range
   [nums n m]
   (loop [i n
-         result 1]
+         ret 1]
     (if (= i (+ m 1))
-        result
-        (recur (+ i 1) (* result (nth nums i))))))
+        ret
+        (recur (+ i 1) (* ret (nth nums i))))))
 
 (defn p8 
   [ls x]
-  (def end (- (count ls) x))
+  (def len (count ls))
   (loop [i 0
+         ret 0]
+    (if (= i (- len x)) 
+        ret
+        (recur (+ i 1) (max ret (multiply-range ls i (- (+ i x) 1)))))))
+
+(def p8arg (filter (fn [x] (not (nil? x)))
+                   (map parse-int 
+                        (map str (seq p8arg*)))))
+
+(println (p8 p8arg 13))
+
+
+;p9
+;special pythagorean triplet
+
+(defn p9
+  [x]
+  (loop [a 1 
+         b 2 
+         c (- x 3)]
+    (cond 
+      (= (* c c) (+ (* b b) (* a a))) (* a b c)
+      (<= c b) (recur (+ a 1) (+ a 2) (- x a a 3)) 
+      :else (recur a (+ b 1) (- c 1)))))
+
+(println (p9 1000))
+
+
+;p10
+;summation of primes
+
+(defn sum-of-primes-below
+  [n]
+  (loop [i 0 
          result 0]
-    (if (= i end) 
-        result
-        (let [mr (multiply-range ls i (- (+ i x) 1))]
-          (if (< result mr)
-              (recur (+ i 1) mr)
-              (recur (+ i 1) result))))))
+    (cond
+      (< n i) result
+      (prime? i) (recur (+ i 1) (+ result i))
+      :else (recur (+ i 1) result))))
 
-(def p8nums (filter (fn [x] (not (nil? x))) (map parse-int (map str (seq p8numstr)))))
+(println (sum-of-primes-below 2000000))
 
-(println (p8 p8nums 13))
+
+;p11
+;largest product in a grid
+
+(def p11arg*
+ "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48")
+
+(defn map-parse-int
+  [ls]
+  (map #(Integer/parseInt %) ls))
+
+(defn rotate90
+  [matrix]
+  (loop [ls matrix ret []]
+    (if (empty? (first ls))
+        ret
+        (recur (map rest ls) (conj ret (reverse (map first ls)))))))
+
+(defn rotate45
+  [matrix]
+  (def cols (count (first matrix)))
+  (def matrix-aux
+    (loop [i 0 ls matrix ret []]
+      (if (empty? ls) 
+          ret 
+          (recur (+ i 1) 
+                 (rest ls) 
+                 (conj ret 
+                      (concat (repeat i 1)
+                              (first ls)
+                              (repeat (- cols 1 i) 1)))))))
+  (rotate90 matrix-aux))
+
+(defn rotate135
+  [matrix]
+  (rotate45 (rotate90 matrix)))
+
+(def test-matrix [[1 2 3] [4 5 6] [7 8 9]])
+(println (rotate90 test-matrix))
+(println (rotate45 test-matrix))
+(println (rotate135 test-matrix))
+
+(defn max-product-series
+  [ls n]
+  (def len (count ls))
+  (if (< len n)
+      (apply * ls)
+      (loop [i 0 ret 0]
+        (if (= (+ i n) len) 
+            ret
+            (recur (+ i 1) (max ret (multiply-range ls i (- (+ i n) 1))))))))
+
+(defn p11
+  [matrix n]
+  (apply max (concat (map (fn [ls] (max-product-series ls n)) matrix)
+                     (map (fn [ls] (max-product-series ls n)) (rotate45 matrix))
+                     (map (fn [ls] (max-product-series ls n)) (rotate90 matrix))
+                     (map (fn [ls] (max-product-series ls n)) (rotate135 matrix)))))
+
+(def p11arg
+  (map map-parse-int 
+       (map (fn [line] (string/split line #" "))
+            (string/split p11arg* #"\n"))))
+
+(println (p11 p11arg 4))
